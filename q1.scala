235b3715a47465f9e49530a2ebd30d7db47ced71
anthonyyoussef01 <30884253+anthonyyoussef01@users.noreply.github.com>
2021-12-19 14:50:55 -0500

    Update q1.scala

--- q1.scala
+++ q1.scala
@@ -1,15 +1,3 @@
-package question1
-
-/*
-Question 1 (AST interpretation)
-
-Write an evaluator (called eval) for the following AST, except for Let and Subs. You should fill in "eval" so it goes
-from a Calc and turns it into an int.
-
-During your pairing interview, you'll extend this to use the Let and Subs statements. For example: 
-Let x equal 3 in y = 3 * x + 4
- */
-
 import question1.Question1.AddReduce
 import scalaz.Reducer
 
